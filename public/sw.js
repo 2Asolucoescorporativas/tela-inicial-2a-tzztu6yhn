0d4973e27e88e917dcd4a16970bf1173cc02983d
Skip <skip@usecurling.com>
2026-07-26 21:27:58 -0300

    feat: Configure app as a PWA

---
+++ public/sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', function (event) {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(self.clients.claim())
+})
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    fetch(event.request).catch(function () {
+      return new Response('Offline', { status: 503, statusText: 'Offline' })
+    }),
+  )
+})
